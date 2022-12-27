b97c1ad2c2bb6f7b01170a52be35608014637c0d
kazu0716 <kazu2055.tennis@gmail.com>
2022-12-27 07:56:31 +0000

    fix: refactoring users

--- apps/routers/users.py
+++ apps/routers/users.py
@@ -12,14 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from google.cloud import spanner
 from google.cloud.spanner_v1.database import Database
 from pydantic import BaseModel, EmailStr, Field, SecretStr
 
-from .utils import (build_insert_query, create_req_tag, get_db,
-                    get_password_hash, get_uuid)
+from .utils import create_req_tag, get_db, get_password_hash, get_uuid
 
 TABLE: str = "Users"
 
@@ -30,56 +32,59 @@ router = APIRouter(
 
 
 class User(BaseModel):
-    name: str
-    mail: EmailStr
-    password: SecretStr = Field(min_length=8, max_length=16)
+    name: str = Field(..., example="hoge")
+    mail: EmailStr = Field(..., example="hoge@example.com")
+    password: SecretStr = Field(min_length=8, max_length=16, example="hogehoge")
 
 
 class UserResponse(BaseModel):
-    user_id: str
-    name: str
-    mail: EmailStr
+    user_id: str = Field(..., example="111111111")
+    name: str = Field(..., example="hoge")
+    mail: EmailStr = Field(..., example="hoge@example.com")
 
 
-@router.get("/", tags=["users"])
+@router.get("/", tags=["users"], response_model=List[UserResponse])
 def read_random_users(db: Database = Depends(get_db)) -> JSONResponse:
-    """
-    Get 1,000 random users for tests initial requests
-    """
+    """Get 1,000 random users for tests initial requests"""
     with db.snapshot() as snapshot:
         query = f"SELECT UserId, Name, Mail From {TABLE} TABLESAMPLE RESERVOIR (1000 ROWS)"
         results = list(snapshot.execute_sql(query))
     return JSONResponse(content=jsonable_encoder([UserResponse(**dict(zip(UserResponse.__fields__.keys(), result))).dict() for result in results]))
 
 
-@router.get("/{user_id}", tags=["users"])
+@router.get("/{user_id}", tags=["users"], response_model=UserResponse, responses={status.HTTP_404_NOT_FOUND: {"description": "User does not found", "content": {"application/json": {"example": {"detail": "The character did not found"}}}}})
 def read_user(user_id: str, db: Database = Depends(get_db)) -> JSONResponse:
-    """
-    Get a user
-    """
+    """Get a user"""
     with db.snapshot() as snapshot:
         query = f"SELECT UserId, Name, Mail From {TABLE} WHERE UserId={user_id}"
         results = list(snapshot.execute_sql(query, request_options={"request_tag": create_req_tag("select", "read_user", "users")}))
+
     if not results:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="The character did not found")
+
     return JSONResponse(content=jsonable_encoder(UserResponse(user_id=results[0][0], name=results[0][1], mail=results[0][2])))
 
 
-@router.post("/", tags=["users"])
+@router.post("/", tags=["users"], response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def create_user(user: User, db: Database = Depends(get_db)) -> JSONResponse:
-    """
-    Create user
-    """
+    """Create user"""
+    def create_user_repository(transaction):
+        query = f"INSERT {TABLE} ( UserId, Name, Mail, Password, CreatedAt, UpdatedAt ) VALUES ( @UserId, @Name, @Mail, @Password, PENDING_COMMIT_TIMESTAMP(), PENDING_COMMIT_TIMESTAMP() )"
+        params = {"UserId": user_id, "Name": user.name, "Mail": user.mail, "Password": hashed_password}
+        params_type = {"UserId": spanner.param_types.INT64, "Name": spanner.param_types.STRING, "Password": spanner.param_types.STRING}
+        transaction.execute_update(query, params=params, param_types=params_type, request_options=request_options)
+
     user_id = get_uuid()
     hashed_password = get_password_hash(user.password.get_secret_value())
-    columns = ("UserId", "Name", "Mail", "Password", "CreatedAt", "UpdatedAt")
-    values = (user_id, user.name, user.mail, hashed_password, "PENDING_COMMIT_TIMESTAMP()", "PENDING_COMMIT_TIMESTAMP()")
     request_options = {"request_tag": create_req_tag("insert", "create_user", "users")}
-    db.run_in_transaction(build_insert_query, TABLE, columns, values, request_options)
+
+    db.run_in_transaction(create_user_repository)
+
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=jsonable_encoder(UserResponse(user_id=user_id, name=user.name, mail=user.mail)))
 
 
 @router.delete("/", tags=["users"])
 def delete_all_users(db: Database = Depends(get_db)) -> JSONResponse:
+    """Delete all users"""
     db.execute_partitioned_dml(f"DELETE FROM {TABLE} WHERE UserId > 0")
     return JSONResponse(content=jsonable_encoder({}))
